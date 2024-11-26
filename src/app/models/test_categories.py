2a7753ad8c0e8e4e4659ae0d04b741161905e910
siamesecat14 <jiyarathi1402@gmail.com>
2024-11-25 17:51:19 -0800

    Fixed the primary key issue in versions.py and test_categories.py

--- src/app/models/test_categories.py
+++ src/app/models/test_categories.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
-
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from src.app.core.database import Base
+from sqlalchemy.orm import relationship
 
 
 class TestCategory(Base):
@@ -9,7 +9,9 @@ class TestCategory(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     test_category_id = Column(String, nullable=False, unique=True)
     products_id=Column(Integer,ForeignKey("products.id"))
+    version_id=Column(Integer, ForeignKey("versions.id"))
 
+    version_for_category=relationship("Version",back_populates="categories")
     product_for_category=relationship("Product",back_populates="testing_categories")
 
     test_result=relationship("TestResult",back_populates="test_category")
--- src/app/models/versions.py
+++ src/app/models/versions.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import Column, Integer, String, ForeignKey
 from src.app.core.database import Base
+from sqlalchemy.orm import relationship
 
 
 class Version(Base):
     __tablename__ = "versions"
     id = Column(Integer, primary_key=True, autoincrement=True)
     version_id = Column(String, nullable=False, unique=True)
+    product_id=Column(Integer,ForeignKey("products.id"))
+
+    product=relationship("Product", back_populates="versions")
+    categories=relationship("TestCategory",back_populates="version_for_categories")
