604cd9a9a4a19a0396817e89b9d410ce62ee20bf
ty-runner <ty.runner02@gmail.com>
2024-11-27 20:59:19 -0800

    versions page populated with API call

--- next_js_frontend/src/app/versions/page.tsx
+++ next_js_frontend/src/app/versions/page.tsx
@@ -1,8 +1,16 @@
-export default function VersionsPage() {
-	return (
-		<div>
-			<h1>Versions Page</h1>
-			<p>This is where our product versions will be displayed, we can show a list using /versions/ GET command to fetch all of the products</p>
-		</div>
-	);
-}
+"use client";
+import React from 'react';
+import VersionList from '../../components/versionList';
+import { API_HOST_BASE_URL } from '../../lib/constants';
+
+const VersionsPage = () => {
+  const apiUrl = `${API_HOST_BASE_URL}/versions/`;
+
+  return (
+    <div>
+      <VersionList apiUrl={apiUrl} />
+    </div>
+  );
+};
+
+export default VersionsPage;
