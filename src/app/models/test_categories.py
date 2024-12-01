622eb0c5298eb1951cb929751781806e50ae111c
ty-runner <ty.runner02@gmail.com>
2024-12-01 14:50:46 -0800

    test fixes

--- src/app/models/test_categories.py
+++ src/app/models/test_categories.py
@@ -1,17 +1,17 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from src.app.core.database import Base
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+
+from src.app.core.database import Base
 
 
 class TestCategory(Base):
     __tablename__ = "test_categories"
     key_id = Column(Integer, primary_key=True, autoincrement=True)
     test_category_id = Column(String, nullable=False, unique=True)
-    products_id=Column(Integer,ForeignKey("products.id"))
-    version_id=Column(Integer, ForeignKey("versions.id"))
+    products_id = Column(Integer, ForeignKey("products.id"))
+    version_id = Column(Integer, ForeignKey("versions.id"))
 
-    version_for_category=relationship("Version",back_populates="categories")
-    product_for_category=relationship("Product",back_populates="testing_categories")
+    version_for_category = relationship("Version", back_populates="categories")
+    product_for_category = relationship("Product", back_populates="testing_categories")
 
-    test_result=relationship("TestResult",back_populates="test_category")
+    test_result = relationship("TestResult", back_populates="test_category")
--- src/app/models/versions.py
+++ src/app/models/versions.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from src.app.core.database import Base
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+
+from src.app.core.database import Base
 
 
 class Version(Base):
     __tablename__ = "versions"
     key_id = Column(Integer, primary_key=True, autoincrement=True)
     version_id = Column(String, nullable=False, unique=True)
-    product_id=Column(Integer,ForeignKey("products.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
 
-    product=relationship("Product", back_populates="versions")
-    categories=relationship("TestCategory",back_populates="version_for_category")
+    product = relationship("Product", back_populates="versions")
+    categories = relationship("TestCategory", back_populates="version_for_category")
