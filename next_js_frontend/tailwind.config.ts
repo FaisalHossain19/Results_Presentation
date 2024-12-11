9be48d3003afa870f5e11c59c083376fc7a1ef24
ty-runner <ty.runner02@gmail.com>
2024-12-11 08:57:42 -0800

    custom image

--- next_js_frontend/tailwind.config.ts
+++ next_js_frontend/tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
 	],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'hero-pattern': "url('/circuit-board.svg')",
+			},
 			colors: {
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
